fe30adf37daaedfd9694f94e1e2a8084aaf60851
aidanliddiard <aidanliddiard@gmail.com>
2022-03-14 14:39:26 -0700

    creating Blogs.js and Main.js

---
+++ src/views/Main.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Main() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+      }
+    };
+  })
+
+  return <div>Main</div>;
+}

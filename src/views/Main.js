f27c3c569a95f241816af950604b29caa4fc4b4f
aidanliddiard <aidanliddiard@gmail.com>
2022-03-14 15:17:12 -0700

    displaying on the page!

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,17 +1,26 @@
-//import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { fetchBlogs } from '../services/Blogs';
 import { useEffect } from 'react';
 
+import BlogCard from '../components/BlogCard/BlogCard';
+
 export default function Main() {
-  //const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  //const [blog, setBlog] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetchBlogs();
-      console.log(resp);
+      setBlogs(resp);
     };
     fetchData();
   }, []);
 
-  return <div>Main</div>;
+  return (
+    <main>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+    </main>
+  );
 }
